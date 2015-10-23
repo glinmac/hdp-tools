2109872f09f035f86de7b4300dc7d7b340204eb4
Guillaume Gardey <glinmac@gmail.com>
2015-10-23 11:28:36 +0100

    Some docs + validation option

--- misc/create_ipa_users.py
+++ misc/create_ipa_users.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python
 """
 Small script to automate user/groups creation and keytab retrieval using FreeIPA
+
+Users file format: a CSV file with at least 2 columns named 'Name', 'Description',
+
+Name,Description,...
+username,account for username,...
+
+Groups file format: a CSV file with at least 2 columns named 'Name', 'Description'
+
+Name,Description,...
+group1,unix group for group1,...
+
+Membership file format: a CSV file with a least 2 columns names 'Group', 'Member'
+
+Group,Member,...
+group1,username,...
+group2,username,...
+
+
+Tested on redhat 6.4/python2.6/freeipa 3.0.0
+
 """
 import csv
 import sys
@@ -9,6 +29,7 @@ from tempfile import mkstemp
 import os
 import re
 import logging
+from collections import defaultdict
 
 logging.getLogger().setLevel(logging.DEBUG)
 
@@ -16,7 +37,7 @@ logging.getLogger().setLevel(logging.DEBUG)
 IPA_SERVER="<YOUR_IPASERVER>"
 REALM="<YOUR_REAML>"
 IPA_PASSWORD="<MASTER_IPA_PASSWORD>"
-INIT_PASSWORD="<INIT_PASSWORD_FOR_USER>"
+INIT_PASSWORD="<INITIAL_PASSWORD_FOR_USER>"
 DEFAULT_PASSWORD="<DEFAULT_PASSWORD_FOR_USER>"
 BASE_GROUPS=[
     'DEFAULT_GROUP1',
@@ -94,7 +115,9 @@ def process_users(users_file, keytab_path):
 
     with open(users_file) as csvfile:
         r = csv.DictReader(csvfile, delimiter=',')
-        for row in r:            
+        for row in r:
+            if row['Name'] == '#':
+                continue
             d = {
                 'username': row['Name'],
                 'description': row['Description'],
@@ -107,6 +130,9 @@ def process_users(users_file, keytab_path):
 
             if not user_exists(d['username']):
                 create_user(d)
+                # When a user is created in IPA, his password autoexpires.
+                # Need to explicitly login or set the expiration to a later date so that the keytab
+                # can be retrieved and used later
                 change_krb_expiration(d['username'])                    
                 create_keytab(d)
                 for g in BASE_GROUPS:
@@ -119,6 +145,8 @@ def process_groups(group_file):
     with open(group_file) as csvfile:
         r = csv.DictReader(csvfile, delimiter=',')
         for row in r:
+            if row['Name'] == '#':
+                continue
             if not group_exists(row['Name']):
                 create_group(row['Name'], row['Description'])
                     
@@ -131,6 +159,49 @@ def process_membership(membership_file):
             if not group_exists(row['Group']):
                 create_group(row['Group'])
             add_group(row['Member'], row['Group'])
+
+def validate_users(users_file):
+    users = []
+    with open(users_file) as csvfile:
+        r = csv.DictReader(csvfile, delimiter=',')
+        for row in r:
+            if row['Name'] == '#':
+                continue
+            if row['Name'] not in users:
+                users.append(row['Name'])
+            else:
+                logging.error('Duplicate user %s in user file %s' % (row['Name'], users_file))
+    return users
+
+def validate_groups(groups_file):
+    groups = []
+    with open(groups_file) as csvfile:
+        r = csv.DictReader(csvfile, delimiter=',')
+        for row in r:
+            if row['Name'] == '#':
+                continue
+            if row['Name'] not in groups:
+                groups.append(row['Name'])
+            else:
+                logging.error('Duplicate user %s in group file %s' % (row['Name'], groups_file))
+    return groups
+
+def validate_membership(membership_file):
+    membership = defaultdict(list)
+    with open(membership_file) as csvfile:
+        r = csv.DictReader(csvfile, delimiter=',')
+        for row in r:
+            if row['Group'] == '#':
+                continue
+            if row['Group'] in membership:
+                if row['Member'] not in membership[row['Group']]:
+                    membership[row['Group']].append(row['Member'])
+                else:
+                    logging.error('Duplicate membership user=%s, group=%s' % (row['Member'], row['Group']))
+            else:
+                membership[row['Group']].append(row['Member'])
+    return membership
+
 
 if __name__ == '__main__':
     from optparse import OptionParser
@@ -145,21 +216,48 @@ if __name__ == '__main__':
                       help='Path to save keytabs', metavar='FILE')
     parser.add_option('--groups', dest='groups',
                       help='List of groups', metavar='FILE')
+    parser.add_option('--validate', dest='validate', action='store_true',
+                      help='Validate input files')
     
     options, args = parser.parse_args()
-    if options.users:
-        if not options.keytab_path:
-            raise RuntimeError('keytab path not specified (--keytab-path)')
-        if not os.path.exists(options.keytab_path):
-            os.makedirs(options.keytab_path)
-        elif not os.path.isdir(options.keytab_path):
-            raise RuntimeError('keytab path already exists and is not a directory')
-                
+
+    if options.validate:
+        users, groups, membership = [], [], defaultdict(list)
+        if options.users:
+            users = validate_users(options.users)
+        if options.groups:
+            groups = validate_groups(options.groups)
+        if options.membership:
+            membership = validate_membership(options.membership)
+
+        if users and groups and membership:
+            for group, val in membership.iteritems():
+                if group not in groups:
+                    logging.warning('group %s not in group file' % group)
+                for user in val:
+                    if user not in users:
+                        logging.warning('user %s not in user file' % user)
+
+        logging.info('%d users going to be created' % len(users))
+        logging.info('%d groups going to be created' % len(groups))
+        logging.info('%d groups will have members updated' % len(membership.keys()))
+
+
+    else:
+        if options.users:
+            if not options.keytab_path:
+                raise RuntimeError('keytab path not specified (--keytab-path)')
+            if not os.path.exists(options.keytab_path):
+                os.makedirs(options.keytab_path)
+            elif not os.path.isdir(options.keytab_path):
+                raise RuntimeError('keytab path already exists and is not a directory')
+
         process_users(options.users, options.keytab_path)
-    
-    if options.groups:
-        process_groups(options.groups)
-    
-    if options.membership:
-        process_membership(options.membership)
+
+        if options.groups:
+            process_groups(options.groups)
+
+        if options.membership:
+            process_membership(options.membership)
+
         
