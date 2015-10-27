9effa48c680bf22dd76c24baacd00f6946b4831c
Guillaume Gardey <glinmac@gmail.com>
2015-10-27 11:58:14 +0000

    Update create_ranger_hive_policies.py

--- ranger/create_ranger_hive_policies.py
+++ ranger/create_ranger_hive_policies.py
@@ -9,6 +9,10 @@ import json
 from httplib import HTTPConnection
 from base64 import b64encode
 import sys
+import logging
+from collections import defaultdict
+
+logging.getLogger().setLevel(logging.DEBUG)
 
 ranger_host = '<HOST>'
 ranger_port = 6080
@@ -49,32 +53,57 @@ def create_policy(data):
        print 'Policy %s created' % data['policyName']
 
 
-with open(sys.argv[1]) as csvfile:
-    reader = csv.DictReader(csvfile, delimiter=',')
-    policy = policy_template
+def create_policies(policy_file, dry_run=False):
 
-    for row in reader:
-        if row['Policy name']:
-            policy['policyName'] = row['Policy name']
-            policy['databases'] =  row['Resource']
-            policy['description'] = description % row['Resource']
-            policy['permMapList'] = [ {
-                'groupList': [row['AD groups']],
-                'permList': []
-            }]
+    policies = {}
+    
+    with open(policy_file) as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=',')
+        current_policy = None
 
-            if row['Select']: policy['permMapList'][0]['permList'].append('SELECT')
-            if row['ALL']: policy['permMapList'][0]['permList'].append('ALL')
-
-        else:
-            policy['permMapList'].append({
-                'groupList': [row['AD groups']],
-                'permList': []
-            })
-
-            if row['Select']: policy['permMapList'][1]['permList'].append('Select')
-            if row['ALL']: policy['permMapList'][1]['permList'].append('ALL')
-
-            create_policy(policy)
+        for row in reader:
+            if row['Policy name'].startswith('#'):
+                continue
             
-            policy = policy_template
+            if row['Policy name'] not in policies and row['Policy name']:
+                current_policy = policy_template.copy()
+                policies[row['Policy name']] = current_policy
+                current_policy['policyName'] = row['Policy name']
+                current_policy['databases'] =  row['Resource']
+                current_policy['description'] = 'CLU policy for %s' % row['Resource']
+                current_policy['permMapList'] = [ {
+                    'groupList': [row['AD groups']],
+                    'permList': []
+                }]
+
+                if row['Select']: current_policy['permMapList'][0]['permList'].append('SELECT')
+                if row['ALL']: current_policy['permMapList'][0]['permList'].append('ALL')
+
+            else:
+                current_policy['permMapList'].append({
+                    'groupList': [row['AD groups']],
+                    'permList': []
+                })
+
+                if row['Select']: current_policy['permMapList'][1]['permList'].append('Select')
+                if row['ALL']: current_policy['permMapList'][1]['permList'].append('ALL')
+    
+    if dry_run:
+        logging.debug('%s policies to be created' % len(policies))
+        for name, policy in policies.iteritems():
+            print name, policy['description'], policy['databases']
+    else:
+        for name, policy in policies.iteritems():
+            create_policy(policy)   
+
+if __name__ == '__main__':
+    from optparse import OptionParser
+    import os.path
+    
+    parser = OptionParser()
+    parser.add_option('--dry-run', dest='dry_run', action='store_true', default=False,
+                      help='Dry run')
+
+    options, args = parser.parse_args()
+    
+    create_policies(args[0], dry_run=options.dry_run)    
